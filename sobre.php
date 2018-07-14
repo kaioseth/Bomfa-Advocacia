c50897024b9a66673f98bca82fdac028b98d64b6
Kaio Nascimento <kaio.seth@gmail.com>
2018-07-14 20:23:50 -0300

    atualizando informações de contato

--- sobre.php
+++ sobre.php
@@ -1,24 +1,29 @@
 <div class="row">
-    <div class="three columns">
+    <div class="two columns">
         <img class="profile-pic"  src="images/profilepic.jpg" alt="" />
     </div>
 
-    <div class="nine columns main-col">
+    <div class="ten columns main-col">
         <h2>Sobre mim</h2>
-        <p>
-            Texto de apresentação
+        <p style="text-align: justify">
+        Advogado atuante, pós-graduando em Direito Politico;
+        Dotado de capacidades de liderança, organização e determinação.
+        Excelente apresentação pessoal, boa dicção, redação e comunicação em geral.
+        Raciocínio rápido, estudioso e grande proficiência na área jurídico-administrativa e política.
+        <br>
+        Domínio total da língua Portuguesa, sendo meu idioma nato, intermediário conhecimento da língua inglesa e espanhola. 
+        Disponibilidade imediata para o exercício da função de advocacia e consultoria, 
+        vez que possui condições para viagens nacionais e internacionais.
         </p>
         <div class="row">
             <div class="columns contact-details">
                 <h2>Para entrar em contato</h2>
                 <p class="address">
-                    <span>Tarson Bomfa</span><br>
-                    <span>
-                        Endereço<br>
-                    </span>
-                    <br>
-                    <span>Telefone</span><br>
-                    <span>E-mail</span>
+                    <span>Tarson Bomfá</span><br>
+                    <span>Celular / Whatsapp:</span> +55 (69) 99265 7112<br>
+                    <span>E-mail:</span> tarsonbomfa@gmail.com<br>
+                    <span>Linkedin:</span>  https://www.linkedin.com/in/tarson-bomf%C3%A1-2a0557144/<br>
+                    <span>Skype:</span> tarsonbomfa@gmail.com<br>
                 </p>
             </div>
         </div>
